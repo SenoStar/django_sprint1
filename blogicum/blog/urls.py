ff26fa56e172b032fe35d3907fc0ba7bd9fdd285
SenoStar <sergejmakarov30043@gmail.com>
2024-08-04 20:11:52 +0300

    fixed version for tests (5 try)

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,10 +5,10 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='posts'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path(
         'category/<slug:category_slug>/',
         views.category_posts,
-        name='category'
+        name='category_posts'
     ),
 ]
